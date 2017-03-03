be3e9dfe4349b35b5df23495203316b0e9259912
Michał Simoniuksztis <m.simoniuksztis@i4b.pl>
2017-03-03 13:32:11 +0100

    added common extension methods

---
+++ src/CqrsPlayground.Common/System/AppDomainExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace CqrsPlayground.Common.System
+{
+    using global::System;
+    using global::System.Collections.Generic;
+    using global::System.Linq;
+    using global::System.Reflection;
+
+    public static class AppDomainExtensions
+    {
+        public static Assembly[] GetAppAssemblies(this AppDomain @this)
+        {
+            return @this.GetAssemblies()
+                .Where(x => x.FullName.StartsWith("ProcentCqrs."))
+                .ToArray();
+        }
+
+        public static IEnumerable<Type> GetAppTypes(this AppDomain @this)
+        {
+            return @this.GetAssemblies()
+                .Where(x => x.FullName.StartsWith("ProcentCqrs."))
+                .SelectMany(x => x.GetTypes());
+        }
+    }
+}
